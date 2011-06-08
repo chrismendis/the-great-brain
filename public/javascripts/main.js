e90ae248ab257626feceeee0515e60b250c59eb8
Edna Piranha <jen@ednapiranha.com>
2011-06-08 15:19:00 -0400

    fixed response from love/hate test

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,4 +4,13 @@ $(function() {
   brain.neuronator = Neuronator();
   brain.neuronator.getNodeList();
   brain.neuronator.pingNode();
+  
+  $('.result li').hover(
+    function() {
+      $(this).find('a').show();
+    },
+    function() {
+      $(this).find('a').hide();
+    }
+  );
 });
--- public/javascripts/neuronator.js
+++ public/javascripts/neuronator.js
@@ -1,47 +1,44 @@
 var Neuronator = function() {
   var node_list = [
     {
-      "node": {
-        "id": 1,
-        "url": "http://love-hate.heroku.com",
-        "root": "/",
-        "receive": "/pong",
-        "send": "/generate",
-        "author": "jen",
-        "personal_url": "http://ednapiranha.com",
-        "tags": "emotion"
-      }
+      "id": 1,
+      "url": "http://localhost:8080",
+      "root": "/",
+      "receive": "/pong",
+      "send": "/generate",
+      "author": "jen",
+      "personal_url": "http://ednapiranha.com",
+      "tags": "emotion"
     }
   ];
   
   var self = {
     getNodeList: function() {
       var result = $(".result");
-      $.each(node_list, function(idx, n) {
-        var node_frame = $("<li data-id=''><iframe src=''></iframe></li>");
-        node_frame.find('iframe').attr('src', n.node.url + n.node.root);
-        node_frame.attr('data-id', n.node.id);
-        result.append(node_frame);
+      var node_frame = $("<li data-id=''><iframe src=''></iframe><a href=''></a></li>");
+      $.each(node_list, function(i, n) {
+        var sibling_node_frame = node_frame.clone();
+        sibling_node_frame.find('a').attr('href', node_list[i].personal_url).text(node_list[i].author);
+        sibling_node_frame.find('iframe').attr('src', node_list[i].url + node_list[i].root);
+        sibling_node_frame.attr('data-id', node_list[i].id);
+        result.append(sibling_node_frame);
       });
     },
     pingNode: function() {
       var ping_state = 1;
-      $.each(node_list, function(idx, n) {
-        console.log('pinging node ...', n.node.url);
-        var url = n.node.url + n.node.send + "?callback=?";
-        console.log(url);
-        $.ajax({
-          type: 'GET',
-          url: url,
-          data: { ping: ping_state },
-          success: function(data) {
-            alert(data);
-            $('.result li[data-id="' + n.node.id + '"]').append("(" + data + ")");
-          },
-          error: function(data, statusText) {
-            console.log(statusText);
-          },
-          dataType: "jsonp"
+      $('.result').everyTime(3000, function(i) {
+        $.each(node_list, function(i, n) {
+        //  console.log('pinging node ...', node_list[i].url);
+          var url = node_list[i].url + node_list[i].send + "?callback=?";
+          $('.result li[data-id="' + node_list[i].id + '"] img').remove();
+          $.getJSON(url, { ping: ping_state }, function(data) {
+            $('.result li[data-id="' + node_list[i].id + '"]').append(data.result);
+          //  console.log(node_list[i].url + node_list[i].receive);
+            $.getJSON(node_list[i].url + node_list[i].receive + "?callback=?", function(data) { 
+              ping_state = parseInt(data.result, 10); 
+            });
+          //  console.log(ping_state);
+          });
         });
       });
     }
