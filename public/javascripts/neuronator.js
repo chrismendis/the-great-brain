709e5f89d243278edbd8029e557a3a0b3a6de842
Edna Piranha <jen@ednapiranha.com>
2011-06-15 19:43:12 -0400

    removed duplicate call to remove p/img/video/audio elements; remove audio/video elements; moved node list to separate file; added brain page - to replace index page with info

--- public/javascripts/neuronator.js
+++ public/javascripts/neuronator.js
@@ -1,131 +1,6 @@
 var Neuronator = function() {
-  var pong_assign_path = "/pong";
-  
   var debug_box_send = $('.debug p.send');
   var debug_box_receive = $('.debug p.receive');
-  
-  var node_list = [
-    {
-      "id": 1,
-      "url": "http://love-hate.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen1",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "image"
-    },
-    {
-      "id": 2,
-      "url": "http://hollow-robot-198.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen2",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "text"
-    },
-    {
-      "id": 3,
-      "url": "http://glowing-sky-477.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen3",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "audio"
-    },
-    {
-      "id": 4,
-      "url": "http://floating-warrior-161.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen4",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "audio"
-    },
-    {
-      "id": 5,
-      "url": "http://hollow-mountain-667.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen5",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "audio"
-    },
-    {
-      "id": 6,
-      "url": "http://floating-warrior-161.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen6",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "text"
-    },
-    {
-      "id": 7,
-      "url": "http://floating-warrior-161.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen6",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "text"
-    },
-    {
-      "id": 8,
-      "url": "http://glowing-sky-477.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen5",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "audio"
-    },
-    {
-      "id": 9,
-      "url": "http://love-hate.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen1",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "image"
-    },
-    {
-      "id": 10,
-      "url": "http://hollow-mountain-667.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen5",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "audio"
-    },
-    {
-      "id": 11,
-      "url": "http://hollow-robot-198.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen2",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "text"
-    },
-    {
-      "id": 12,
-      "url": "http://floating-warrior-161.heroku.com",
-      "root": "/",
-      "receive": "/pong",
-      "send": "/generate",
-      "author": "jen2",
-      "personal_url": "http://ednapiranha.com",
-      "tags": "text"
-    }
-  ];
   
   var randomizeNode = function() {
     var node_id = Math.ceil(Math.random()*node_list.length);
@@ -146,21 +21,22 @@ var Neuronator = function() {
     },
     pingNode: function(ping_state, node_id) {    
       var node_reference = parseInt(node_id, 10) - 1;
+      
       console.log('node reference ' + node_reference);
+      
       var url_send = node_list[node_reference].url + node_list[node_reference].send + "?callback=?";
       var url_receive = node_list[node_reference].url + node_list[node_reference].receive + "?callback=?";
       
-      $('.result li img, .result li p').remove();
+      $('.result li img, .result li p, .result li audio, .result li video').remove();
       
       debug ? debug_box_send.text('pinging node ... ' + node_list[node_reference].url + ' ' + node_id) : '';
-      $('.result li[data-id="' + node_list[node_reference].id + '"] img').remove();
-      $('.result li[data-id="' + node_list[node_reference].id + '"] p').remove();
+
       console.log('send url ' + url_send);
       console.log('ping node id ' + node_id);
+      
       $.getJSON(url_send, { ping: ping_state }, function(data) {
         $('.result li[data-id="' + node_id + '"]').append(data.result);
         $.getJSON(url_receive, function(d) {
-          console.log('receive url ' + url_receive);
           pong_state = parseInt(d.result, 10);
           brain.neuronator.pongNode(pong_state, node_list[node_reference].id);
           debug ? debug_box_receive.text("node_id, ping value = " + node_list[node_reference].id + ", " + ping_state) : '';
@@ -172,8 +48,10 @@ var Neuronator = function() {
     },
     pongNode: function(pong_state, node_id) {
       pong_state = parseInt(pong_state, 10);
+      
       console.log('pong state ' + pong_state);
       console.log('pong node id ' + node_id);
+      
       brain.neuronator.updatePingNode(pong_state);
     }
   };
---
+++ public/javascripts/nodes.js
@@ -0,0 +1,162 @@
+var node_list = [
+  {
+    "id": 1,
+    "url": "http://love-hate.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen1",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "image"
+  },
+  {
+    "id": 2,
+    "url": "http://hollow-robot-198.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen2",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 3,
+    "url": "http://glowing-sky-477.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen3",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 4,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen4",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 5,
+    "url": "http://hollow-mountain-667.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen5",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 6,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen6",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 7,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen6",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 8,
+    "url": "http://glowing-sky-477.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen5",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 9,
+    "url": "http://love-hate.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen1",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "image"
+  },
+  {
+    "id": 10,
+    "url": "http://hollow-mountain-667.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen5",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 11,
+    "url": "http://hollow-robot-198.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen2",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 12,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen2",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 13,
+    "url": "http://hollow-mountain-667.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen5",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  },
+  {
+    "id": 14,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen6",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 15,
+    "url": "http://floating-warrior-161.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen6",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "text"
+  },
+  {
+    "id": 16,
+    "url": "http://glowing-sky-477.heroku.com",
+    "root": "/",
+    "receive": "/pong",
+    "send": "/generate",
+    "author": "jen5",
+    "personal_url": "http://ednapiranha.com",
+    "tags": "audio"
+  }
+];
