f7541ca9389ce3895293e8629894a5891a14722a
Edna Piranha <jen@ednapiranha.com>
2011-06-17 10:28:11 -0400

    switched to snow background

--- public/javascripts/neuronator.js
+++ public/javascripts/neuronator.js
@@ -10,11 +10,10 @@ var Neuronator = function() {
   var self = {
     getNodeList: function() {
       var result = $(".result");
-      var node_frame = $("<li data-id='' class='free'><iframe src=''></iframe><a href='' target='_blank'></a><p></p></li>");
+      var node_frame = $("<li data-id='' class='free'><a href='' target='_blank'></a><p></p></li>");
       $.each(node_list, function(i, n) {
         var sibling_node_frame = node_frame.clone();
         sibling_node_frame.find('a').attr('href', node_list[i].personal_url).text(node_list[i].author);
-        sibling_node_frame.find('iframe').attr('src', node_list[i].url + node_list[i].root);
         sibling_node_frame.attr('data-id', node_list[i].id);
         result.append(sibling_node_frame);
       });
@@ -22,13 +21,14 @@ var Neuronator = function() {
     pingNode: function(ping_state, node_id) {    
       try {
         var node_reference = parseInt(node_id, 10) - 1;
+        var free_nodes = $('.result li.free');
       
         console.log('node reference ' + node_reference);
       
         var url_send = node_list[node_reference].url + node_list[node_reference].send + "?callback=?";
         var url_receive = node_list[node_reference].url + node_list[node_reference].receive + "?callback=?";
       
-        $('.result li img, .result li p, .result li audio, .result li video').remove();
+        free_nodes.find('img, p, audio, video').remove();
       
         debug ? debug_box_send.text('pinging node ... ' + node_list[node_reference].url + ' ' + node_id) : '';
 
@@ -36,7 +36,7 @@ var Neuronator = function() {
         console.log('ping node id ' + node_id);
       
         $.getJSON(url_send, { ping: ping_state }, function(data) {
-          $('.result li[data-id="' + node_id + '"]').addClass('busy').removeClass('free').append(data.result);
+          $('.result li[data-id="' + node_id + '"]').addClass('busy').removeClass('free').html(data.result);
           $.getJSON(url_receive, function(d) {
             pong_state = parseInt(d.result, 10);
             brain.neuronator.pongNode(pong_state, node_list[node_reference].id, node_list[node_reference].time);
@@ -52,7 +52,7 @@ var Neuronator = function() {
     },
     pongNode: function(pong_state, node_id, time) {
       pong_state = parseInt(pong_state, 10);
-      
+      $('.result li[data-id="' + node_id + '"]').removeClass('busy').addClass('free');
       console.log('pong state ' + pong_state);
       console.log('pong node id ' + node_id);
       
